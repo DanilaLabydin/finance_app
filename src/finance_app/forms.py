3239f961d60d476ca92db0276b764a2b9671fcc9
DanilaLabydin <labydin2605@mail.ru>
2024-02-06 23:47:18 +0300

    delete unwanted files

--- src/finance_app/forms.py
+++ src/finance_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import FoodProducts, Stuff, SStuff, FFoodProducts, Category
+from .models import Category, Spent
 
 
 class CategoryForm(forms.ModelForm):
@@ -7,26 +7,8 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ["name", "description"]
 
-        
-class FoodProductsForm(forms.ModelForm):
+
+class SpentForm(forms.ModelForm):
     class Meta:
-        model = FoodProducts
-        fields = ["title", "amount", "price", "date"]
-
-
-class StuffForm(forms.ModelForm):
-    class Meta:
-        model = Stuff
-        fields = ["title", "price", "date"]
-
-
-class SStuffForm(forms.ModelForm):
-    class Meta:
-        model = SStuff
-        fields = ["title", "price", "date"]
-
-
-class FFoodForm(forms.ModelForm):
-    class Meta:
-        model = FFoodProducts
-        fields = ["title", "price", "amount", "date"]
+        model = Spent
+        fields = ["name", "price", "description", "category"]
--- src/finance_app/models.py
+++ src/finance_app/models.py
@@ -1,30 +1,16 @@
 import uuid
 from django.db import models
 from django.utils import timezone, dateformat
-from datetime import datetime
 
 
 def get_time():
     return dateformat.format(timezone.now(), "Y-m-d")
 
 
-# class User(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     name = models.CharField(max_length=100, null=False)
-#     time_create = models.DateField(default = get_time)
-#     time_update = models.DateField(default = get_time)
-
-
-# class UserSettings(models.Model):  ## write smth about user restrictions for their spents
-#     ...
-
-## class Currency(models.Model) ## persistent currancy data (rubles, dollars, etc)
-
 class Category(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.AutoField(primary_key=True, editable=False)
     name = models.CharField(max_length=100, null=False)
     description = models.CharField(max_length=500, null=True)
-    # user = models.UUIDField(null=True)
     archive = models.BooleanField(default=False)
     time_create = models.DateField(default=get_time)
     time_update = models.DateField(default=get_time)
@@ -34,12 +20,11 @@ class Category(models.Model):
 
 
 class Spent(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.AutoField(primary_key=True, editable=False)
     name = models.CharField(max_length=100, null=True)
     price = models.FloatField(null=False)
     description = models.CharField(max_length=500, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    # user = models.UUIDField(null=True)
     archive = models.BooleanField(default=False)
     time_create = models.DateField(default=get_time)
     time_update = models.DateField(default=get_time)
@@ -47,111 +32,3 @@ class Spent(models.Model):
     def __str__(self):
         return f"{self.name}: {self.price}"
 
-
-def get_month():
-    today = datetime.today()
-    datem = datetime(today.year, today.month, 1)
-    output = ""
-    if "-01-" in str(datem):
-        output = "January"
-    elif "-02-" in str(datem):
-        output = "February"
-    elif "-03-" in str(datem):
-        output = "March"
-    elif "-04-" in str(datem):
-        output = "April"
-    elif "-05-" in str(datem):
-        output = "May"
-    elif "-06-" in str(datem):
-        output = "June"
-    elif "-07-" in str(datem):
-        output = "July"
-    elif "-08-" in str(datem):
-        output = "August"
-    elif "-09-" in str(datem):
-        output = "September"
-    elif "-10-" in str(datem):
-        output = "October"
-    elif "-11-" in str(datem):
-        output = "November"
-    else:
-        output = "December"
-    return str(output)
-
-
-class SStuff(models.Model):
-    title = models.CharField(max_length=100)
-    price = models.IntegerField()
-    date = models.DateTimeField(default=get_time)
-    month = models.TextField(default=get_month)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ["date"]
-
-
-class FFoodProducts(models.Model):
-    title = models.CharField(max_length=100)
-    amount = models.IntegerField()
-    price = models.IntegerField()
-    date = models.DateTimeField(default=get_time)
-    month = models.TextField(default=get_month)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ["date"]
-
-
-class Stuffs(models.Model):
-    title = models.CharField(max_length=100)
-    price = models.IntegerField()
-    date = models.DateTimeField(default=get_time)
-    month = models.TextField(default=get_month)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ["date"]
-
-
-class FoodProducts(models.Model):
-    title = models.CharField(max_length=100)
-    amount = models.IntegerField()
-    price = models.IntegerField()
-    date = models.DateTimeField(default=get_time)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ["date"]
-
-
-class Rest(models.Model):
-    title = models.CharField(max_length=100)
-    amount = models.IntegerField()
-    price = models.IntegerField()
-    date = models.DateTimeField(default=get_time)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ["date"]
-
-
-class Stuff(models.Model):
-    title = models.CharField(max_length=100)
-    price = models.IntegerField()
-    date = models.DateTimeField(default=get_time)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ["date"]
