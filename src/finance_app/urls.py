a50c9ffda857f3fc9e4f53bb00835c9c43bc9a0f
DanilaLabydin <labydin2605@mail.ru>
2024-02-08 23:08:38 +0300

    add read and update op

--- src/finance_app/urls.py
+++ src/finance_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index, get_user_category, add_user_category, get_user_spents, add_user_spent
-from . import views
+from .views import (index, get_user_category, add_user_category,
+                     get_user_spents, add_user_spent, get_user_spent_by_id,
+                     get_user_category_by_id, update_user_spent)
 
 
 urlpatterns = [
@@ -8,5 +9,9 @@ urlpatterns = [
     path("categories/", get_user_category, name="get_all_categories"),
     path("spents/", get_user_spents, name="get_all_spents" ),
     path("categories/new_category", add_user_category, name="new_category"),
-    path("spents/new_spent", add_user_spent, name="new_spent")
+    path("spents/new_spent", add_user_spent, name="new_spent"),
+    path("spents/<int:spent_id>", get_user_spent_by_id, name="get_spent"),
+    path("categories/<int:category_id>", get_user_category_by_id, name="get_category"),
+    path("spents/<int:spent_id>/update", update_user_spent, name="update_spent")
+
 ]
--- src/finance_app/views.py
+++ src/finance_app/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render, redirect
+
+from django.shortcuts import (get_object_or_404,
+                              render,
+                              HttpResponseRedirect)
+from django.http import Http404
+
 from .models import Category, Spent
 from .forms import CategoryForm, SpentForm
 
@@ -24,10 +29,12 @@ def get_user_spents(request):
     except Exception as e:
         return
     headers = ["id","name", "price", "description", "category"]
-    body = [dict(id=spent_obj.id, name=spent_obj.name, price=spent_obj.price, description=spent_obj.description, category=spent_obj.category) for spent_obj in spents]
+    body = [dict(
+        id=spent_obj.id, name=spent_obj.name, price=spent_obj.price,
+        description=spent_obj.description, category=spent_obj.category,
+        time_create=spent_obj.time_create, time_update=spent_obj.time_update) for spent_obj in spents]
     context = {"headers": headers, "body": body}
     return render(request, "finance_app/spents.html", context)
-
 
 
 def add_user_category(request):
@@ -54,16 +61,45 @@ def add_user_spent(request):
     return render(request, "finance_app/add_spent.html", context)
 
 
-def get_user_spent_by_id(request):
-    pass
+def get_user_spent_by_id(request, spent_id):
+    try:
+        spent = Spent.objects.get(pk=spent_id)
+        context = {"spent": spent}
+    except Spent.DoesNotExist:
+        raise Http404("Spent with that id doesn't exist")
+    return render(request, "finance_app/get_spent.html", context)
 
 
-def get_user_category_by_id(request):
-    pass
+def get_user_category_by_id(request, category_id):
+    try:
+        category = Category.objects.get(pk=category_id)
+        context = {"category": category}
+    except Category.DoesNotExist:
+        raise Http404("Category with that id doesn't exist")
+    return render(request, "finance_app/get_category.html", context)
 
 
-def update_user_spent(request):
-    pass
+def update_user_spent(request, spent_id):
+    # dictionary for initial data with 
+    # field names as keys
+    context ={}
+ 
+    # fetch the object related to passed id
+    obj = get_object_or_404(Spent, id = spent_id)
+ 
+    # pass the object as instance in form
+    form = SpentForm(request.POST or None, instance = obj)
+ 
+    # save the data from the form and
+    # redirect to detail_view
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/")
+ 
+    # add form dictionary to context
+    context["form"] = form
+ 
+    return render(request, "finance_app/update_view.html", context)
 
 
 def update_user_category(request):
@@ -72,5 +108,10 @@ def update_user_category(request):
 
 def delete_user_category(request):
     pass
+
+
+def delete_user_spent(request):
+    pass
+
 
 
