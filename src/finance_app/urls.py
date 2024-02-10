74982b9c478f8b7064b477e4d5c0aea7575d6f35
DanilaLabydin <labydin2605@mail.ru>
2024-02-10 14:19:48 +0300

    run black, finish basic crud operations

--- src/finance_app/urls.py
+++ src/finance_app/urls.py
@@ -1,17 +1,37 @@
 from django.urls import path
-from .views import (index, get_user_category, add_user_category,
-                     get_user_spents, add_user_spent, get_user_spent_by_id,
-                     get_user_category_by_id, update_user_spent)
+from .views import (
+    index,
+    get_user_category,
+    add_user_category,
+    get_user_spents,
+    add_user_spent,
+    get_user_spent_by_id,
+    get_user_category_by_id,
+    update_user_spent,
+    update_user_category,
+    delete_user_category,
+    delete_user_spent,
+)
 
 
 urlpatterns = [
     path("", index, name="main_page"),
     path("categories/", get_user_category, name="get_all_categories"),
-    path("spents/", get_user_spents, name="get_all_spents" ),
+    path("spents/", get_user_spents, name="get_all_spents"),
     path("categories/new_category", add_user_category, name="new_category"),
     path("spents/new_spent", add_user_spent, name="new_spent"),
     path("spents/<int:spent_id>", get_user_spent_by_id, name="get_spent"),
     path("categories/<int:category_id>", get_user_category_by_id, name="get_category"),
-    path("spents/<int:spent_id>/update", update_user_spent, name="update_spent")
-
+    path("spents/<int:spent_id>/update", update_user_spent, name="update_spent"),
+    path(
+        "categories/<int:category_id>/update",
+        update_user_category,
+        name="update_category",
+    ),
+    path("spents/<int:spent_id>/delete", delete_user_spent, name="delete_spent"),
+    path(
+        "categories/<int:category_id>/delete",
+        delete_user_category,
+        name="delete_category",
+    ),
 ]
--- src/finance_app/views.py
+++ src/finance_app/views.py
@@ -1,7 +1,4 @@
-
-from django.shortcuts import (get_object_or_404,
-                              render,
-                              HttpResponseRedirect)
+from django.shortcuts import get_object_or_404, render, HttpResponseRedirect
 from django.http import Http404
 
 from .models import Category, Spent
@@ -16,9 +13,12 @@ def get_user_category(request):
     try:
         categories = Category.objects.all()
     except Exception as e:
-        return 
+        return
     headers = ["id", "name", "description"]
-    body = [dict(id=cat_obj.id, name=cat_obj.name, description=cat_obj.description) for cat_obj in categories]
+    body = [
+        dict(id=cat_obj.id, name=cat_obj.name, description=cat_obj.description)
+        for cat_obj in categories
+    ]
     context = {"headers": headers, "body": body}
     return render(request, "finance_app/categories.html", context)
 
@@ -28,11 +28,19 @@ def get_user_spents(request):
         spents = Spent.objects.all()
     except Exception as e:
         return
-    headers = ["id","name", "price", "description", "category"]
-    body = [dict(
-        id=spent_obj.id, name=spent_obj.name, price=spent_obj.price,
-        description=spent_obj.description, category=spent_obj.category,
-        time_create=spent_obj.time_create, time_update=spent_obj.time_update) for spent_obj in spents]
+    headers = ["id", "name", "price", "description", "category"]
+    body = [
+        dict(
+            id=spent_obj.id,
+            name=spent_obj.name,
+            price=spent_obj.price,
+            description=spent_obj.description,
+            category=spent_obj.category,
+            time_create=spent_obj.time_create,
+            time_update=spent_obj.time_update,
+        )
+        for spent_obj in spents
+    ]
     context = {"headers": headers, "body": body}
     return render(request, "finance_app/spents.html", context)
 
@@ -64,7 +72,7 @@ def add_user_spent(request):
 def get_user_spent_by_id(request, spent_id):
     try:
         spent = Spent.objects.get(pk=spent_id)
-        context = {"spent": spent}
+        context = {"spent": spent, "spent_id": spent_id}
     except Spent.DoesNotExist:
         raise Http404("Spent with that id doesn't exist")
     return render(request, "finance_app/get_spent.html", context)
@@ -73,45 +81,72 @@ def get_user_spent_by_id(request, spent_id):
 def get_user_category_by_id(request, category_id):
     try:
         category = Category.objects.get(pk=category_id)
-        context = {"category": category}
+        context = {"category": category, "category_id": category_id}
     except Category.DoesNotExist:
         raise Http404("Category with that id doesn't exist")
     return render(request, "finance_app/get_category.html", context)
 
 
 def update_user_spent(request, spent_id):
-    # dictionary for initial data with 
-    # field names as keys
-    context ={}
- 
+    context = {}
+
     # fetch the object related to passed id
-    obj = get_object_or_404(Spent, id = spent_id)
- 
+    obj = get_object_or_404(Spent, id=spent_id)
+
     # pass the object as instance in form
-    form = SpentForm(request.POST or None, instance = obj)
- 
+    form = SpentForm(request.POST or None, instance=obj)
+
     # save the data from the form and
     # redirect to detail_view
     if form.is_valid():
         form.save()
-        return HttpResponseRedirect("/")
- 
+        return HttpResponseRedirect("/spents")
+
     # add form dictionary to context
     context["form"] = form
- 
-    return render(request, "finance_app/update_view.html", context)
+    context["spent_id"] = spent_id
+
+    return render(request, "finance_app/update_spent.html", context)
 
 
-def update_user_category(request):
-    pass
+def update_user_category(request, category_id):
+    context = {}
+
+    obj = get_object_or_404(Category, id=category_id)
+    form = CategoryForm(request.POST or None, instance=obj)
+
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/categories")
+
+    context["form"] = form
+    context["category_id"] = category_id
+    return render(request, "finance_app/update_category.html", context)
 
 
-def delete_user_category(request):
-    pass
+def delete_user_category(request, category_id):
+    context = {}
+    obj = get_object_or_404(Category, id=category_id)
+
+    if request.method == "POST":
+        # delete object
+        obj.delete()
+        # after deleting redirect to
+        # home page
+        return HttpResponseRedirect("/categories")
+
+    return render(request, "finance_app/delete_category.html", context)
 
 
-def delete_user_spent(request):
-    pass
+def delete_user_spent(request, spent_id):
+    context = {}
+    obj = get_object_or_404(Spent, id=spent_id)
 
+    if request.method == "POST":
+        # delete object
+        obj.delete()
+        # after deleting redirect to
+        # home page
+        return HttpResponseRedirect("/spents")
 
-
+    return render(request, "finance_app/delete_spent.html", context)
